21b5dc35eb0affef95c33a187e46c49093f83e07
rian <rianluccas08@gmail.com>
2023-08-19 22:18:55 -0300

    refactor: change prisma filter return

--- src/exceptions-filter/prisma.exception-filter.ts
+++ src/exceptions-filter/prisma.exception-filter.ts
@@ -6,17 +6,21 @@ export class PrismaExceptionFilter implements ExceptionFilter {
   catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
     const context = host.switchToHttp();
     const response = context.getResponse();
+    const request = context.getRequest();
 
     if (exception.code === 'P2025') {
       return response.status(404).json({
         statusCode: 404,
         message: exception.message,
+        timestamp: new Date().toISOString(),
+        path: request.url,
       });
     }
 
     return response.status(500).json({
       statusCode: 500,
-      message: 'Internal Server Error',
+      timestamp: new Date().toISOString(),
+      path: request.url,
     });
   }
 }
